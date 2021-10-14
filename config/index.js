392b11253e5b646417dc21605a2b3aebc6a0f56e
karen-yareli <karenyarelicotoalfonso8@gmail.com>
2021-10-14 15:41:23 -0500

    actlualizando

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const config = {
     port: process.env.PORT,
     cors: process.env.CORS,
     dbUser: process.env.DB_USER,
-    dbPassword: process.env.BD_PASSWORD,
+    dbPassword: process.env.DB_PASSWORD,
     dbHost: process.env.DB_HOST,
     dbName: process.env.DB_NAME
 }
