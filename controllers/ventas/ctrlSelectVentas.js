a7f2e2bda6d800db14462e3cfff80767cc19ec88
Culebro05 <05dianacruz@gmail.com>
2021-11-08 03:52:08 -0600

    cambios

--- controllers/ventas/ctrlSelectVentas.js
+++ controllers/ventas/ctrlSelectVentas.js
@@ -2,9 +2,9 @@ const { selectVentas } = require('../../services/ventas/selectVentas')
 
 async function ctrlSelectVentas(req, res){
   try{
-  const { orden } = req.query
+  const { emailCliente } = req.query
   console.log('Desde el controladores')
-  const ventas = await selectVentas( orden )
+  const ventas = await selectVentas( emailCliente )
   return res.status(200).send(ventas)
   }catch(error) {
     return res.status(error.status || 500 ).send({message: error.message})
--- controllers/ventas/ctrlUpdateVentas.js
+++ controllers/ventas/ctrlUpdateVentas.js
@@ -2,10 +2,10 @@ const { updateVentas} = require('../../services/ventas/updateVentas')
 
 async function ctrlUpdateVentas(req, res){
   try{
-  const { vendedor,  descripcion, fecha, tipodepago, total} = req.body
-  const { orden } = req.params
+  const { vendedor,  orden, descripcion, fecha, tipodepago, total} = req.body
+  const { emailCliente } = req.params
   console.log('Desde el controlador')
-  const ventas = await updateVentas({vendedor, orden, descripcion, fecha, tipodepago, total})
+  const ventas = await updateVentas({vendedor, orden, emailCliente, descripcion, fecha, tipodepago, total})
   return res.status(200).send(ventas)
   }catch(error) {
     return res.status(error.status || 500 ).send({message: error.message})
