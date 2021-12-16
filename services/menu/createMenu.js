19c9a556f11b60d65791292e10fc085321ff9882
karen-yareli <karenyarelicotoalfonso8@gmail.com>
2021-12-16 02:37:15 -0600

    act

--- services/menu/createMenu.js
+++ services/menu/createMenu.js
@@ -1,17 +1,18 @@
 const Menu = require('../../models/menu')
 
-async function createMenu ({categoria, name, price}){
-  try{
-    const menuCreated = await Menu.create({
-      categoria,
-      name,
-      price
-  })
+async function CreateMenu({ name, user_id, precio, descripcion }) {
+  try {
+       const productCreated = await product.create({
+            name,
+            precio,
+            user_id,
+            descripcion
+       });
+       return productCreated;
+  } catch (error) {
+       return { message: error.message };
+  }
 
-  console.log(menuCreated)
-  return menuCreated
-  }catch(error){
-  return {message: error.message}
   }
 }
-module.exports = {createMenu}
+  module.exports = {createMenu}
